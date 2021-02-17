359d423f8169ed16d4a0948a08600d60dddbcf1b
Max <mchen210@uottawa.ca>
2021-02-17 13:30:36 -0500

    add gravatar icon and gravatar redirect to the account management page

--- src/pages/ManageAccount.tsx
+++ src/pages/ManageAccount.tsx
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
+import { useContext } from 'react';
+import Gravatar from 'react-gravatar';
+import { useHistory } from 'react-router-dom';
+import { Button } from 'rsuite';
+import { User } from '../utils/api/User';
 
 export default function ManageAccount() {
+  const user = useContext(User);
+  const history = useHistory();
   return (
     <Fragment>
       <h1>Account</h1>
       <p>Update or delete your Democracy account.</p>
       <br />
       <p>
-        In development! Tools will be added here soon to manage your account.
+      <Gravatar email={user?.user.email}/>
+      <br />
+      <Button onClick={()=>{window.location.href=`https://en.gravatar.com/`}}>manage you accout with gravatar</Button>
       </p>
     </Fragment>
   );
