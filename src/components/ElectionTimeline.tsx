bb6b36453f5efd92eab07b4eac2898f9393480ef
Max <mchen210@uottawa.ca>
2021-02-23 20:50:17 -0500

    extract text from election timeline

--- src/components/ElectionTimeline.tsx
+++ src/components/ElectionTimeline.tsx
@@ -2,6 +2,7 @@ import React, { FC, Fragment } from 'react';
 import { Icon, Loader, Timeline } from 'rsuite';
 import { ElectionDetails } from '../utils/api/ElectionManagement';
 import moment from 'moment';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
   election: ElectionDetails;
@@ -18,6 +19,7 @@ const ElectionTimeline: FC<Props> = ({ election }) => {
         </div>
       </Fragment>
     );
+  const [t] = useTranslation()
   return (
     <Fragment>
       <div style={{ marginBottom: 20 }}>
@@ -26,28 +28,28 @@ const ElectionTimeline: FC<Props> = ({ election }) => {
             <p>
               <b>{moment(election.submission_start_time).format(timeformat)}</b>
             </p>
-            <p>Application period begins</p>
+            <p>{t('electionTimeline.appBtn')}</p>
           </Timeline.Item>
           <Timeline.Item dot={<Icon icon="pencil" size="2x" />}>
             <p>
               <b>{moment(election.submission_end_time).format(timeformat)}</b>
             </p>
-            <p>Application period ends</p>
+            <p>{t('electionTimeline.appEnd')}</p>
           </Timeline.Item>
           <Timeline.Item>
-            <p>Review of applications by election manager</p>
+            <p>{t('electionTimeline.review')}</p>
           </Timeline.Item>
           <Timeline.Item dot={<Icon icon="list" size="2x" />}>
             <p>
               <b>{moment(election.voting_start_time).format(timeformat)}</b>
             </p>
-            <p>Voting period begins</p>
+            <p>{t('electionTimeline.voteBegin')}</p>
           </Timeline.Item>
           <Timeline.Item dot={<Icon icon="check-square-o" size="2x" />}>
             <p>
               <b>{moment(election.voting_end_time).format(timeformat)}</b>
             </p>
-            <p>Voting period ends</p>
+            <p>{t('electionTimeline.voteEnd')}</p>
           </Timeline.Item>
         </Timeline>
       </div>
